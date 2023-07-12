fbac26dd87b796ecf4eec0fb48691d11646ec9ac
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-07-12 15:50:01 +0200

    add tecnology scaffolding

---
+++ database/seeders/TecnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tecnology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TecnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tecnologies = ['bootstrap', 'laravel', 'vue', 'vite'];
+
+        foreach($tecnologies as $tecnology){
+            $newTecnology = new Tecnology();
+            $newTecnology->name = $tecnology("name") ;
+            $newTecnology->save();
+
+        }
+
+    }
+}
